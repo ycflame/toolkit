684fcd3469d1472dee5a3c479b37f831a44f6f9c
yangchao <yangchao@admaster.com.cn>
2013-09-10 14:40:19 +0800

    add path install

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env zsh
+
+add_to_path ()
+{
+    if [[ "$PATH" =~ (^|:)"${1}"(:|$) ]]
+    then
+        return 0
+    fi
+    echo "export PATH=$PATH:${1}" >> ~/.zshrc
+    source ~/.zshrc
+}
+
+tool_path=$(pwd)/bin
+add_to_path $tool_path
 
 cp vimrc ~/.vimrc
-rsync -rtvu --delete --exclude '.git' vim/ ~/.vim/
+rsync -rtvu --exclude '.git' vim/ ~/.vim/
