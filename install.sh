4860c28aad0b94e6086498e8bf6796f8f0623a72
yangchao <yangchao@admaster.com.cn>
2013-09-13 14:34:25 +0800

    remove PATH editing

--- install.sh
+++ install.sh
@@ -1,17 +1,4 @@
-#!/usr/bin/env zsh
-
-add_to_path ()
-{
-    if [[ "$PATH" =~ (^|:)"${1}"(:|$) ]]
-    then
-        return 0
-    fi
-    echo "export PATH=$PATH:${1}" >> ~/.zshrc
-    source ~/.zshrc
-}
-
-tool_path=$(pwd)/bin
-add_to_path $tool_path
+#!/usr/bin/env bash
 
 cp vimrc ~/.vimrc
 rsync -rtvu --exclude '.git' vim/ ~/.vim/
