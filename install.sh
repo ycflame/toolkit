26d027cab0fdfa2042252bbb8eeae9ce2bc0eb13
YCFlame <yangchao.cs@gmail.com>
2014-01-28 17:13:51 +0800

    keep pyc files in destination directory

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ git submodule init
 git submodule update
 cp ~/.vimrc{,.bak}
 cp vimrc ~/.vimrc
-rsync -avzhu --delete-excluded --exclude $1 '.git' vim/ ~/.vim/
+rsync -avzhu --filter='P *.pyc' --delete-excluded --exclude $1 '.git' vim/ ~/.vim/
 cp templates/* ~/.vim/bundle/vim-template/templates
