c4b86f4ccae914090424150eb24c9d724d118c95
YCFlame <yangchao.cs@gmail.com>
2014-01-28 17:07:05 +0800

    change rsync option to delete .git

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ git submodule init
 git submodule update
 cp ~/.vimrc{,.bak}
 cp vimrc ~/.vimrc
-rsync -rtvu --exclude $1 '.git' vim/ ~/.vim/
+rsync -avzhu --delete-excluded --exclude $1 '.git' vim/ ~/.vim/
 cp templates/* ~/.vim/bundle/vim-template/templates
